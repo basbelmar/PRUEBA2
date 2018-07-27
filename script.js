f5e516af98acb2839780f747a396579024e1ebd4
basbelmar <galvanize.bas@gmail.com>
2018-07-26 22:11:56 -0400

    Operativo

---
+++ script.js
@@ -0,0 +1,49 @@
+$(function() {
+  // Handler for .ready() called.
+
+  //eliminar tweet
+$('#timeline').on('click','.eliminar', function(){
+	 	console.log($(this).parent().remove());
+})
+
+ $('#writer').on('submit', function(event){
+	    var text= $("#textotw").val();
+	  	event.preventDefault();
+
+	  	$('#timeline').prepend(
+	  	'<div class="tweetposted container">'
+       + '<img src="img/3.jpg" alt="Avatar" style="width:25px">'
+       +'<p>' + text + '</p>'
+       +'<a class="eliminar" href="#"><i class="fas fa-trash"></i></a>'
+       +'<a class="like" href="#">'
+         +'<i class="ico fas fa-thumbs-up">0</i>'
+         +'</div>')
+	  	});
+
+
+
+
+
+
+$('.like').on('click','.ico', function(){
+$(this).css('color','blue');
+var contador= $('.ico').text();
+contador= parseInt(contador);
+contador= contador+1;
+console.log(contador);
+
+$(this).html ('<i>'+contador+'</i>')
+
+
+
+
+});
+
+
+$("#image").on("change", function(e){
+$(".crearperfil img").attr("src", "img/" + $(this).val());
+});
+
+
+});
+
